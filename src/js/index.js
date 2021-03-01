6ec8436aa4f502d70c8a9c9b49c0825a4c5f9b76
Deni <azureswastika@gmail.com>
2021-03-01 17:12:14 +0300

    Tags filter

---
+++ src/js/index.js
@@ -0,0 +1,40 @@
+const container = $(".posts");
+const tags = $(".tags");
+const converter = new showdown.Converter();
+
+
+function render_posts(tag = null) {
+    $.getJSON("assets/posts.json", function (posts) {
+        let tag_arr = []
+        container.html("");
+        tags.html('<p class="tag">all</p>');
+        posts.forEach((post) => {
+            post.tags.forEach((tag) => {
+                if (tag_arr.indexOf(tag) == -1) {
+                    tag_arr.push(tag)
+                }
+            })
+        })
+        if (tag != null & tag != "all") {
+            posts = posts.filter((obj) => obj.tags.indexOf(tag) != -1)
+        }
+        posts.forEach((post) => {
+            $.get(post.file, function (text) {
+                post["data"] = text
+                let el_div = $('<div class="post"/>').appendTo(container)
+                let html = converter.makeHtml(post.data)
+                $(html).appendTo(el_div)
+                $("<hr/>").appendTo(el_div)
+            });
+        })
+        tag_arr.forEach((tag) => {
+            $(`<p class="tag">${tag}</p>`).appendTo(tags)
+        })
+    });
+}
+
+$(document).on('click', '.tag', function () {
+    render_posts($(this)[0].innerText)
+});
+
+$(document).ready(render_posts())
